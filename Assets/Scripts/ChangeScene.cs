846bd9d118e31a865185177b190a9ca0c53b4147
Jean-Baptiste LY <ly.jeanbaptiste@gmail.com>
2020-11-03 21:06:58 +0100

    pique

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class ChangeScene : MonoBehaviour
 {
+	public Object sceneToLoad;
+
     void OnCollisionEnter(Collision col)
     {
     	if(col.gameObject.tag == "Player")
     	{
-    		SceneManager.LoadScene("Level 2");
+    		SceneManager.LoadScene(sceneToLoad.name);
     	}
     }
 
---
+++ Assets/Scripts/PiqueTeleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiqueTeleport : MonoBehaviour
+{
+    public float picDamage = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject, 10);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+    	if(col.gameObject.tag == "Player")
+    	{
+    		col.transform.position = new Vector3(14.9796f, 0.0f, 4.187284f);
+    		col.gameObject.GetComponent<PlayerInventory>().ApplyDamage(picDamage);
+    	}
+    }
+}
