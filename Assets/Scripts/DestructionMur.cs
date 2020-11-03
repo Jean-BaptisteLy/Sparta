83a14f354134a32cb5eab8298091f7a276406f66
Jean-Baptiste LY <ly.jeanbaptiste@gmail.com>
2020-11-04 00:45:26 +0100

    manque la barre de vie du boss

---
+++ Assets/Scripts/DestructionMur.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionMur : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+    	if(col.gameObject.tag == "Player")
+    	{
+    		Destroy(gameObject);
+    	}
+    }
+}
